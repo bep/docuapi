05f25192feafba33c8136d33c8652f5453c7cc03
Bjørn Erik Pedersen <bjorn.erik.pedersen@gmail.com>
2016-10-18 00:06:15 +0200

    Add basic exampleSite
    * Add SASS to CSS compile step
    * And refactor the static layout.
    * And add a ugly but basic layout

--- update-slate.go
+++ update-slate.go
@@ -7,6 +7,9 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strings"
+
+	libsass "github.com/wellington/go-libsass"
 )
 
 func main() {
@@ -17,20 +20,35 @@ func main() {
 		log.Fatal(err)
 	}
 
+	// Store Slate statics below /static/slate
+	slateTarget := filepath.Join(pwd, "static", "slate")
+
+	if err := os.RemoveAll(slateTarget); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := os.MkdirAll(slateTarget, os.ModePerm); err != nil {
+		log.Fatal(err)
+	}
+
 	fmt.Println("Update Slate from source ...")
 
-	slateDir, err := ioutil.TempDir("", "hugo-slate")
+	slateSource, err := ioutil.TempDir("", "hugo-slate")
 
 	if err != nil {
 		log.Fatal("Failed to create tmpdir: ", err)
 	}
 
-	if err := cloneSlateInto(slateDir); err != nil {
+	if err := cloneSlateInto(slateSource); err != nil {
 		log.Fatal("Failed to clone Slate: ", err)
 	}
 
-	if err := replaceSlateSourcesInTheme(slateDir, pwd); err != nil {
+	if err := replaceSlateSourcesInTheme(slateSource, slateTarget); err != nil {
 		log.Fatal("Failed to move Slate sources: ", err)
+	}
+
+	if err := compileSassSources(filepath.Join(slateSource, "source", "stylesheets"), filepath.Join(slateTarget, "stylesheets")); err != nil {
+		log.Fatal("Failed compile SASS stylesheets: ", err)
 	}
 }
 
@@ -43,7 +61,6 @@ func cloneSlateInto(dir string) error {
 }
 
 var staticSlateDirs = []string{
-	"stylesheets",
 	"javascripts",
 	"images",
 	"fonts",
@@ -52,9 +69,40 @@ var staticSlateDirs = []string{
 func replaceSlateSourcesInTheme(source, target string) error {
 	for _, staticDir := range staticSlateDirs {
 		fmt.Println("Move", staticDir)
-		if err := os.Rename(filepath.Join(source, "source", staticDir), filepath.Join(target, "static", staticDir)); err != nil {
+		if err := os.Rename(filepath.Join(source, "source", staticDir), filepath.Join(target, staticDir)); err != nil {
 			return err
 		}
+	}
+	return nil
+}
+
+func compileSassSources(source, target string) error {
+	os.MkdirAll(target, os.ModePerm)
+	fis, err := ioutil.ReadDir(source)
+	if err != nil {
+		return err
+	}
+	for _, fi := range fis {
+		if strings.HasPrefix(fi.Name(), "_") {
+			continue
+		}
+		targetName := strings.TrimSuffix(fi.Name(), ".scss")
+		fmt.Println("Compile", fi.Name(), "to", targetName)
+
+		cssFile, err := os.Create(filepath.Join(target, targetName))
+		if err != nil {
+			return err
+		}
+
+		comp, err := libsass.New(cssFile, nil, libsass.Path(filepath.Join(source, fi.Name())))
+		if err != nil {
+			return err
+		}
+
+		if err := comp.Run(); err != nil {
+			return err
+		}
+
 	}
 	return nil
 }
