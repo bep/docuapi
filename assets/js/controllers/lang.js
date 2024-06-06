fafa11a083848e85ce8477e1d9986a1575acb1c0
German Cabarcas <gcc@unironical.com>
2024-06-06 12:46:54 -0500

    Mitigates layout shift on switching programming language tab

--- assets/js/controllers/lang.js
+++ assets/js/controllers/lang.js
@@ -11,19 +11,29 @@ const toggleCodeblockVisibility = function (lang, visible) {
 export function newLangController() {
 	return {
 		tabs: [],
+        contentChildren: [],
 		changeLanguage: function (index) {
 			debug('changeLanguage', index);
+            const anchor = this.contentChildren.find(el => {
+                return el.getBoundingClientRect().top > 0
+            })
+            const anchorValue = anchor.getBoundingClientRect().top;
 			for (let i = 0; i < this.tabs.length; i++) {
 				let isActive = i === index;
 				this.tabs[i].active = isActive;
 
 				toggleCodeblockVisibility(this.tabs[i].key, isActive);
 			}
+            const updatedAnchorElementValue = anchor.getBoundingClientRect().top;
+            anchorValue !== updatedAnchorElementValue && window.scrollBy(0, updatedAnchorElementValue);
 		},
 		initLangs: function (tabs) {
 			debug('initLangs', tabs);
 			tabs[0].active = true;
 			this.tabs = tabs;
+            this.contentChildren = Array.from(document.querySelector('.page-wrapper .content').children)
+                .filter(el => el.tagName !== 'BLOCKQUOTE')
+                .filter(el => !el.classList.contains('highlight'))
 
 			return this.$nextTick(() => {
 				this.changeLanguage(0);
